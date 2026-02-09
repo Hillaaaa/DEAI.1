7d1041062d6772f1a2bb3d80fb8b7a20d8c6e6ec
ADMIN <ocholahillary430@gmail.com>
2026-02-09 21:54:56 +0300

    final

---
+++ contacts_manager.py
@@ -0,0 +1,23 @@
+contact_list =[{
+    "name": "hillary",
+    "phone":"1",
+    "email":"win"},{
+    "name": "willy",
+    "phone": "1",
+    "email": "win"}]
+search_name  = "alex"
+matching_contacts =[]
+
+
+for contact in contact_list:
+    if search_name.lower() == contact["name"].lower():
+        matching_contacts.append(contact)
+if len(matching_contacts)==0:
+    print("Contact not found")
+else:
+    print(f"{len(matching_contacts)} contacts found")
+    print(matching_contacts)
+unique_names= set(contact["name"] for contact in contact_list)
+unique_contacts = set(contact["phone"] for contact in contact_list)
+print(unique_names)
+print(unique_contacts)
