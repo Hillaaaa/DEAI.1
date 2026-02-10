1bcaddc5334432ec16b52e56efaf158b7f7b7027
ADMIN <ocholahillary430@gmail.com>
2026-02-10 19:35:18 +0300

    final

--- min_max_normalization.py
+++ min_max_normalization.py
@@ -2,6 +2,6 @@ import numpy as np
 def data_normalize(data):
     data = (data - np.min(data))/(np.max(data) - np.min(data))
     return data
-data = np.array([5, 15, 25, 35, 45])
+data = np.array([5, 15, 25, 35, 45,55])
 print(data_normalize(data))
 
